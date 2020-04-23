54fa5373a373fb4c01f1e6e48a29aca9ae80286b
Timinii <61969494+Timinii@users.noreply.github.com>
2020-04-23 20:36:23 +0100

    Update Number game task 3.py

--- Number game task 3.py
+++ Number game task 3.py
@@ -5,38 +5,51 @@ import string
 
 # assign default prompt.
 prompt = '> '
+# create variable to count guesses.
 guess_count = 0
+# assign default boolean
 guessed_correct = False
 
 
+# define choose difficulty function.
 def choose_difficulty():
     # set global variables.
     global attempts
 
-
+    # create an infinite loop.
     while True:
+        # print message to user.
         print("\nChoose a difficulty between easy, medium, and hard.")
         # collect user input into variable.
         choice = input(prompt)
 
+        # set conditional statements to run code block.
         if choice == "easy":
             print(f"You chose {choice} mode.")
+            # call 'easy' function.
             easy()
+            # break out of while loop.
             break
-
+        # set conditional statements to run code block.
         elif choice == "medium":
             print(f"You chose {choice} mode.")
+            # call 'medium' function.
             medium()
-
+            # break out of while loop.
             break
+        # set conditional statements to run code block.
         elif choice == "hard":
             print(f"You chose {choice} mode.")
+            # call 'hard' function.
             hard()
+            # break out of while loop.
             break
+        # set conditional statements to run code block.
         else:
             print("Invalid entry.")
 
 
+# define 'easy' function.
 def easy():
     # set global variables.
     global attempts, guess_range, secret_num
@@ -51,6 +64,7 @@ def easy():
     print(f"You have {attempts} attempts.")
 
 
+# define 'medium' function.
 def medium():
     # set global variables.
     global attempts, guess_range, secret_num
@@ -80,14 +94,21 @@ def hard():
     print(f"You have {attempts} attempts.")
 
 
+# define 'start' function.
 def start():
+    # create a conditional while loop
     while guessed_correct == False:
+        # print message to user.
         print("\nWelcome to a modified numbers guessing game!")
+        # call 'choose difficulty' function.
         choose_difficulty()
+        # call 'guess num' function.
         guess_num()
+        # call 'try again' function.
         try_again()
 
 
+# create 'guess num' function.
 def guess_num():
     # set global variables.
     global guess_count, attempts, guessed_correct, guess_range
@@ -96,47 +117,63 @@ def guess_num():
     while guess_count < attempts and not guessed_correct:
 
         try:
+            # print message to users.
             print(f"\nGuess a number between {guess_range[0]} and {guess_range[-1]}.")
             # collect user input into 'guess' variable
             guess = int(input(prompt))
 
+            # create conditional if-statement.
             if guess == secret_num:
+                # print message to user.
                 print("You got it right!")
                 # assign boolean value to 'guessed_correct' variable.
                 guessed_correct = True
+            # create conditional elif-statement.
             elif guess != secret_num and guess in guess_range:
+                # print message to user.
                 print("\nThat was wrong")
+                # increment 'guess_count'.
                 guess_count += 1
+                # print message to user.
                 print(f"{attempts - guess_count} attempts left")
+            # create conditional elif-statement.
             elif guess not in guess_range:
+                # print message to user.
                 print("You guessed out of your scope. Try again!")
-
+            # # create conditional else-statement.
 
         except ValueError:
             print("Invalid entry. Please input a number")
     if not guess_count < attempts:
         print('\nGame over')
 
+# define 'try_again' function.
 def try_again():
     # set global variables.
-    global guessed_correct, guesscount
+    global guessed_correct, guess_count
 
+    # create infinite while loop.
     while True:
+        # print message to user.
         print("\nWould you like to play again? yes/no")
         # collect user input into 'go_again' variable.
         go_again = input(prompt)
+        # create conditional if-statement.
         if go_again == "yes" or go_again == "y":
             # assign boolean value to 'guessed_correct' variable.
             guessed_correct = False
-            # assign value to guess_count.
+            # assign value to gues_count.
             guess_count = 0
             # call the 'start' function.
             start()
-
+        # create conditional elif-statement.
         elif go_again == "no" or go_again == "n":
             print("\nThanks for playing! Come back some other time.\n")
+            # call exit function with no error message.
             exit(0)
+        # create conditional else-statement.
         else:
+            # print message to user.
             print("Invalid entry.")
 
 
